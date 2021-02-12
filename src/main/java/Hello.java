136c3d8bf4e9dfbaae7a9c5c703692447c6305f0
B21 Saidullo <dsaidullo@gmail.com>
2021-02-12 01:10:33 -0800

    movie1 branch more work here

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hey There, B21");
         System.out.println("B21 future SDET");
+        // watching the rest of movie in the bedroom
     }
 
 }
