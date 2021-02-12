b8a873a084f56229a34f5f5cff68f1a3d553d1b7
B21 Saidullo <dsaidullo@gmail.com>
2021-02-12 01:56:59 -0800

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hey There, B21");
-        System.out.println("B21 future SDET");
+        System.out.println("Hey There, B22");
+        System.out.println("B21 future SDET learning Git");
         // watching the rest of movie in the bedroom
+        // movie on the phone for 10 minutes
     }
 
 }
